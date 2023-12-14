fa2e68c0fdc1020114c73e9e1e6bfc54733d0060
Hasan Smajlović <smajlovic.hasan02@gmail.com>
2023-12-14 16:41:25 +0100

    Responsive Register/Log form

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,55 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "./globals.css";
 
+/* @client */
 function Register() {
+  const [isRegister, setIsRegister] = useState(true);
+
   return (
-    <>
+    <div>
       <form>
         <div id="registerForm">
-          <h1> Sign up </h1>
-          <input
-            type="text"
-            id="fullName"
-            name="fullName"
-            placeholder="Full Name"
-          />
+          <h1>{isRegister ? "Register" : "Login"}</h1>
+          {isRegister && (
+            <>
+              <input
+                type="text"
+                id="fullName"
+                name="fullName"
+                placeholder="Full Name"
+              />
+              <input
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+              />
+            </>
+          )}
           <input type="text" id="email" placeholder="Email" />
           <input type="password" id="password" placeholder="Password" />
-          <input
-            type="password"
-            id="confirmPassword"
-            placeholder="Confirm Password"
-          />
-          <button className="signUp" type="button">
-            Sign up
+          <button
+            className="signUp"
+            type="button"
+            onClick={() => setIsRegister(!isRegister)}
+          >
+            {isRegister ? "Sign up" : "Login"}
           </button>
           <p>
-            Already have an account? <a href="google.com"> Log in </a>
+            {isRegister
+              ? "Already have an account? "
+              : "Don't have an account? "}
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsRegister(!isRegister);
+              }}
+            >
+              {isRegister ? "Log in" : "Register"}
+            </a>
           </p>
-          <hr></hr>
+          <hr />
           <button className="facebookLogin" type="button">
             Login with Facebook
           </button>
@@ -35,10 +58,8 @@ function Register() {
           </button>
         </div>
       </form>
-    </>
+    </div>
   );
 }
 
-export default function Home() {
-  return <Register />;
-}
+export default Register;
